3681b0dc598ed59ff9a0a93a2cd6e15b2fdc9e02
samprity <samprity.kashyap@gmail.com>
2016-12-11 12:06:33 -0800

    initial commit

---
+++ client/data/comments.js
@@ -0,0 +1,217 @@
+const comments = {
+  "BAhvZrRwcfu":[
+    {
+      "text":"Beautiful",
+      "user": "heavymetaladam"
+    }
+  ],
+  "BAcyDyQwcXX":[
+    {
+      "text":"100!",
+      "user": "jdaveknox"
+    },
+    {
+      "text":"amazing!",
+      "user": "jdaveknox"
+    },
+    {
+      "text":"You inspire me!",
+      "user": "wesbos"
+    },
+    {
+      "text":"Keep up the good work",
+      "user": "willowtreemegs"
+    }
+  ],
+  "BAPIPRjQce9":[
+    {
+      "text":"Love it",
+      "user": "rrsimonsen"
+    },
+    {
+      "text":"Beautiful gallery",
+      "user": "terzisn"
+    },
+    {
+      "text":"100",
+      "user": "henrihelvetica"
+    },
+    {
+      "text":"Sick shot!",
+      "user": "brentoage"
+    },
+    {
+      "text":"wow",
+      "user": "haaps"
+    },
+    {
+      "text":"Lucky you!",
+      "user": "gobananna"
+    },
+    {
+      "text":"What camera do you use?",
+      "user": "wesbos"
+    }
+  ],
+  "BAF_KY4wcRY":[
+    {
+      "text":"Looking great",
+      "user": "axcdnt"
+    },
+    {
+      "text":":)",
+      "user": "henrihelvetica"
+    }
+  ],
+  "_4jHytwcUA":[
+    {
+      "text":"that is the sound of success!",
+      "user": "mdxprograms"
+    }
+  ],
+  "_zbaOlQcbn":[
+    {
+      "text":"Love!",
+      "user": "pathiebert"
+    }
+  ],
+  "_rmvQfQce8":[
+    {
+      "text":"A+",
+      "user": "mrjoedee"
+    },
+    {
+      "text":"Cool",
+      "user": "jrtashjian"
+    }
+  ],
+  "_ep9kiQcVy":[
+    {
+      "text":"Wow",
+      "user": "sophie_and_sadie"
+    }
+  ],
+  "_XpJcrwcSn":[
+    {
+      "text":"You live in heaven",
+      "user": "pmgllc"
+    }
+  ],
+  "_KnU7MwceA":[
+
+  ],
+  "_HMejJQcY5":[
+    {
+      "text":"👌",
+      "user": "t_volpe"
+    },
+    {
+      "text":"Nice shot, mister!",
+      "user": "imagesofthisandthat"
+    }
+  ],
+  "_Fq2zmwcaz":[
+    {
+      "text":"😍",
+      "user": "melsariffodeen"
+    },
+    {
+      "text":"Very cool!",
+      "user": "ka11away"
+    }
+  ],
+  "_A2r0aQcfD":[
+    {
+      "text":"Uhu!",
+      "user": "lucascaixeta"
+    }
+  ],
+  "1rhFawccs":[
+    {
+      "text":"What's your lighting source?",
+      "user": "seth_mcleod"
+    }
+  ],
+  "pjx-gQcVi":[
+
+  ],
+  "oTZ0zQcWt":[
+    {
+      "text":"Love!",
+      "user": "_lindersss"
+    }
+  ],
+  "mxKQoQcQh":[
+
+  ],
+  "hZh6IQcfN":[
+    {
+      "text":"What do we gotta do to get some :)? @wesbos",
+      "user": "jonasbad"
+    },
+    {
+      "text":"Might drop by today - real quick. Trade you an illegal pin for these? Lol. @wesbos",
+      "user": "henrihelvetica"
+    },
+    {
+      "text":"I'm with @jonasbad on this. What we gotta do? :D",
+      "user": "datamoshr"
+    },
+    {
+      "text":"@jonasbad @datamoshr I'll post them up on my blog soon!",
+      "user": "wesbos"
+    },
+    {
+      "text":"Want",
+      "user": "kamuelafranco"
+    },
+    {
+      "text":"want!",
+      "user": "josemanuelxyz"
+    },
+    {
+      "text":"#Top",
+      "user": "gabrielvieira.me"
+    }
+  ],
+  "fasqlQceO":[
+    {
+      "text":"Where's lux at? 💤?",
+      "user": "henrihelvetica"
+    },
+    {
+      "text":"Love this house during the holidays! And all other times of the year...",
+      "user": "danielleplas"
+    }
+  ],
+  "VBgtGQcSf":[
+    {
+      "text":"@dogsandbrew",
+      "user": "likea_bos"
+    },
+    {
+      "text":"Next on my list!",
+      "user": "tomwalsham"
+    },
+    {
+      "text":"Is that from collective arts ?",
+      "user": "trevorb_91"
+    }
+  ],
+  "FpTyHQcau":[
+    {
+      "text":"@kaitbos  that vest!!! 😍",
+      "user": "courtneyeveline"
+    },
+    {
+      "text":"I just love her! @kaitbos",
+      "user": "kalibrix"
+    },
+    {
+      "text":"@courtneyeveline I know! My friend gave it to her and I wanted a matching one but only Lux can pull it off. She's so fancy 😉",
+      "user": "kaitbos"
+    }
+   ]
+};
+
+export default comments;
---
+++ client/data/posts.js
@@ -0,0 +1,90 @@
+const posts = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"Winter walks #MustBeVancouver #pnwwonderland #myVanLife",
+      "likes":356,
+      "id":"1161022966406956503",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14624702_1844270842458103_523621702396018688_n.jpg"
+   },
+   {
+      "code":"BAcJeJrQca9",
+      "caption":"Snow! ⛄️🌨❄️ #snowpacalypse #snowcouver",
+      "likes":259,
+      "id":"1160844458347054781",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/15275540_355671038141511_940112682613211136_n.jpg"
+   },
+   {
+      "code":"BAF_KY4wcRY",
+      "caption":"Everything looks better in white! #snowcouver #snowpacalypse",
+      "likes":179,
+      "id":"1154606670337393752",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/15306666_599229496942334_7468170803087933440_n.jpg"
+   },
+   {
+      "code":"BAPIPRjQce9",
+      "caption":"Christmas vibes #ferriswheel #winter",
+      "likes":121,
+      "id":"1157179863266871229",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/15275620_136619506824671_7444920014810906624_n.jpg"
+   },
+   {
+      "code":"-hZh6IQcfN",
+      "caption":"Oh Deer! #roadtrips #travelgram",
+      "likes":666,
+      "id":"1126293663140399053",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/15043518_1303295206369425_9132895054661156864_n.jpg"
+   },
+   {
+      "code":"-B3eiIwcYV",
+      "caption":"Canadian deserts are so pretty! #osoyoos #canadiandesert #narcity #dailyhivevan #letsgosomewhere",
+      "likes":313,
+      "id":"1117418173361145365",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14730486_620114221530385_1530909387210620928_n.jpg"
+   },
+   {
+      "code":"BAhvZrRwcfu",
+      "caption":"Those summer days! #okanagan #upick #okanaganvalley #valley #beautifuldestinations #explorebc #exploreokanagan #landscape",
+      "likes":3930,
+      "id":"1162418651480049646",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14659411_191994347873581_7801300612733206528_n.jpg"
+   },
+   {
+      "code":"BAAJqbOQcW5",
+      "caption":"Take me back! #mountrainier #mountains #naturephotography #explorewashington #usa'",
+      "likes":1440,
+      "id":"1152964002473690553",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14701167_1738120373115930_2376997019441954816_n.jpg"
+   },
+   {
+      "code":"_4jHytwcUA",
+      "caption":"Portland vibes 👶🏼⌨",
+      "likes":6342,
+      "id":"1150824171912152320",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14733713_1240313086026943_5728574512652353536_n.jpg"
+   },
+   {
+      "code":"_zbaOlQcbn",
+      "caption":"Fall #pnwwonderland",
+      "likes":52,
+      "id":"1149382879529256679",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/s750x750/sh0.08/e35/14693818_1083640298422901_6148881399513350144_n.jpg"
+   },
+   {
+      "code":"_rmvQfQce8",
+      "caption":"Fall colors on fleek! #fall #myVanLife 🍜",
+      "likes":35,
+      "id":"1147180903383025596",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14716395_344902315859091_8431038776486133760_n.jpg"
+   },
+   {
+      "code":"_rmvQfQce8",
+      "caption":"Mushrooms! #keepportlandweird #travelgram #travelTheWorld",
+      "likes":35,
+      "id":"1147180903383025596",
+      "display_src":"https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/14677391_1129895087094041_5042532437205188608_n.jpg"
+   },
+
+];
+
+
+export default posts;
